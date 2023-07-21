fa3e441b2bbda778829ba8cbc4ad6d7d500a22a9
wagnerbizarro <wagnerbzr@gmail.com>
2023-07-21 14:03:03 -0300

    Finalizado modulo 2 resolvendo issue fixed #3

---
+++ 2_conceitos_basicos/5_palavras_reservadas/index.php
@@ -0,0 +1,13 @@
+<?php
+/*Algumas palavras reservadas:
+ * echo, insteadof, else, interface, namespace, pow
+ * _DIR_, _FILE_, endif, print, private, protected, and
+ * require, public, as, break, case, for, finally, switch, throw e etc;
+ */
+
+//ERROR
+ function echo() {
+ 	return true;
+ }
+
+?>
