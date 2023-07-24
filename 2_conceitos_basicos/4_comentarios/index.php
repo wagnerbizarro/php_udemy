e4d6f756d9e3800fa3e19bf72796367e66421472
wagnerbizarro <wagnerbzr@gmail.com>
2023-07-24 17:27:01 -0300

    Metade do estudo sobre tipos de dados

---
+++ 2_conceitos_basicos/4_comentarios/index.php
@@ -0,0 +1,16 @@
+<?php
+
+//Este é um comentário
+echo "Comentario 1<br>";
+
+#Outra forma de fazer comentário
+echo "Comentario 2<br>";
+
+/*
+	Cometário
+	com 
+	multi
+	linhas
+ */
+echo "Comentario de multilinhas<br>";
+?>
---
+++ 3_tipos_de_dados/5_checando_float/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$a = "teste";
+$b = 12.8;
+
+if(is_float($a)) {
+	echo "$a É float! <br>";
+}
+
+
+if(is_float($b)) {
+	echo "$b É float! <br>";
+}
+
+
+?>
