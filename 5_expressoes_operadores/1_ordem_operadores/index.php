0405f59e3c94c7352fa5838527e078fbf6107b0a
wagnerbizarro <wagnerbzr@gmail.com>
2023-08-15 15:10:02 -0300

    ate a aula 65

---
+++ 5_expressoes_operadores/1_ordem_operadores/index.php
@@ -0,0 +1,27 @@
+<?php
+
+//Operações matematicas seguem a ordem da matematica
+//primeiro multiplicação e divisão, depois soma e subtração
+//Para alterar a ordem precisa usar ()
+
+ echo 3 + 2 * 5;
+ echo "<br>";
+
+ echo (3 + 2) * 5;
+ echo "<br>";
+
+ echo 5 + 2 / 10;
+ echo "<br>";
+
+ $a = 5;
+ $b = 2;
+ $c = 10;
+
+ echo $a + $b / $c;
+ echo "<br>";
+ echo $c + $b /$a;
+ echo "<br>";
+
+ $d = $a * $b * $c;
+ 
+?>
