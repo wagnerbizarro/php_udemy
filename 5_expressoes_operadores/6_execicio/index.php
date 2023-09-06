78446d16a12b302d4cd0bacb04a251c7cde5aab6
wagnerbizarro <wagnerbzr@gmail.com>
2023-09-06 14:50:56 -0300

    Feat: Operadores Aritmeticos

---
+++ 5_expressoes_operadores/6_execicio/index.php
@@ -0,0 +1,20 @@
+<?php
+/*
+- Crie um arquivo PHP
+- Crie uma operação com cada um dos operadores básicos
+- Cada operação deve estar em uma variável diferente
+- Imprima cada uma das etapas
+- Ex:soma-> multiplicação->divisão->subtração
+*/
+
+$soma = 10 + 5;
+$subtracao = $soma - 10;
+$divisao = $soma / $subtracao;
+$multiplicacao = $soma * $divisao;
+
+echo "Resultado da soma: $soma<br>";
+echo "Resultado da subtracao: $subtracao<br>";
+echo "Resultado da divisao: $divisao<br>";
+echo "Resultado da multiplicacao: $multiplicacao<br>";
+
+?>
