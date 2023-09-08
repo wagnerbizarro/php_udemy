163fd28b5a1191abe7db54b6f8721ac545432a15
wagnerbizarro <wagnerbzr@gmail.com>
2023-09-08 14:45:06 -0300

    Feat: Feito ate concatenacao

---
+++ 5_expressoes_operadores/11_exercicio/index.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ Crie um arquivo PHP;
+ Crie uma variavel saudação, nome e outra sobrenome;
+ Imprima com echo a concatenação de saudação, nome e sobrenome;
+
+*/
+
+$saudacao = "Seja bem vindo";
+$nome = "João";
+$sobrenome = "da Silva";
+
+echo $saudacao ." ". $nome ." ". $sobrenome;
+
+?>
---
+++ 5_expressoes_operadores/8_exercicio/index.php
@@ -0,0 +1,16 @@
+<?php
+
+  /*
+  Crie um arquivo PHP;
+  Teste o operador de resto em duas divisões;
+  Uma não exata e outra exta;
+  */
+
+ 
+$exata = 50 % 5;
+$notexata = 50 % 7;
+
+echo "Exata: $exata<br>";
+echo "Não Exata: $notexata<br>"; 
+
+?>
