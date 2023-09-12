5bee9f4fee41e8dad838fe9cc71579ff4ea10dfe
wagnerbizarro <wagnerbzr@gmail.com>
2023-09-12 16:12:24 -0300

    Feito ate operador de igualdade

---
+++ 5_expressoes_operadores/14_Exercicio_igualdade/index.php
@@ -0,0 +1,29 @@
+<?php
+/* 
+Crie uma operação que retorne falso com igualdade;
+Crie uma operação que retorne verdadeiro com igualdade;
+*/
+
+$casa = "casa";
+$carro = "carro";
+$numero1 = 10;
+$numero2 = 10;
+
+if($casa == $carro) {
+    echo "true";
+}
+else {
+    echo "false";
+}
+
+echo "<br>";
+
+if($numero1 == $numero2) {
+    echo "true";
+}
+else {
+    echo "false";
+}
+
+
+
