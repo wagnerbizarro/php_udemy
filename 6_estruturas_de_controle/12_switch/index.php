452549fd74eb39db23f12aeeef1310e80066404a
wagnerbizarro <wagnerbzr@gmail.com>
2023-09-27 15:37:20 -0300

    Feat: Finalizado o modulo 6 !

---
+++ 6_estruturas_de_controle/12_switch/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$x = 5;
+
+switch($x) {
+
+    case 0:
+        echo "X é igual a 0 <br>";
+        break;
+    
+    case 1:
+        echo "X é igual a 1 <br>";
+        break;
+    
+    default:
+    echo "X não é igual a nehum dos valores mencionados <br>";
+}
+
