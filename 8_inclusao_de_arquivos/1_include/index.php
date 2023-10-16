14cc3e363f99686a57ed586628687a72c5a892e6
wagnerbizarro <wagnerbzr@gmail.com>
2023-10-16 10:24:25 -0300

    Feat: Iniciei o modulo 8!

---
+++ 8_inclusao_de_arquivos/1_include/index.php
@@ -0,0 +1,8 @@
+<?php
+//Include: Se o arquivo não existir, não gera erro fatal, apenas um aviso warning
+
+include "teste.php";
+
+?>
+<p>Após o include</p>
+<p>Imprimindo c: <?php echo $c; ?></p>
---
+++ 8_inclusao_de_arquivos/1_include/teste.php
@@ -0,0 +1,4 @@
+<?php
+    $c = 5;
+?>
+<h1>Testando o include</h1>
