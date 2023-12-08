ab0e4c66f8b3ba324bd2bd475caa13c1772c2887
wagnerbizarro <wagnerbzr@gmail.com>
2023-12-08 17:21:39 -0300

    feat: Iniciado modulo 9 funcao

---
+++ 9_Funcao/3_exercicio1/index.php
@@ -0,0 +1,17 @@
+<?php
+
+# Crie um array com strings;
+# Utilize a função implode no arra;
+# Primeiro argumento: ","
+# Segundo argumento: o seu array
+# Atribua  a invocação da função a uma variável
+# Exiba o resultado
+
+$arr = ["Teste", "Olá", "Balão", "Janela", "Planeta"];
+
+$teste = implode(", ", $arr);
+
+echo $teste;
+
+
+?>
---
+++ 9_Funcao/4_exercio2/index.php
@@ -0,0 +1,21 @@
+<?php
+
+# Crie uma função;
+# define três variáveis numéricas dentro dela;
+# Exiba a multiplicação destes números com um echo;
+
+function multiplicacao() {
+
+    $x = 5;
+    $y = 4.12;
+    $z = 8;
+
+    $resultado = $x *$y *$z;
+
+    echo $resultado . "<br>";
+
+}
+
+multiplicacao();
+
+?>
