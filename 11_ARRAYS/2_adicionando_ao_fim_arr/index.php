e62c506c40eb91e97e778bc73e4e422395aa6e96
wagnerbizarro <wagnerbzr@gmail.com>
2023-12-27 09:22:57 -0300

    feat: iniciado o modulo 11

---
+++ 11_ARRAYS/2_adicionando_ao_fim_arr/index.php
@@ -0,0 +1,24 @@
+<?php
+
+$arr = [1, 2, 3];
+
+$arr[] = 4;
+
+print_r($arr);
+echo "<br>";
+
+$arr[] = 5;
+
+print_r($arr);
+echo "<br>";
+
+
+// Segundo array
+
+$arr2 = [];
+$arr2[] = 1;
+
+print_r($arr2);
+echo "<br>";
+
+?>
---
+++ 11_ARRAYS/3_range/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$arr = range(1, 10); //do valor 1 ate o 10
+
+print_r($arr);
+echo "<br>";
+
+$arr2 = range(0, 1000, 100); //do 0 ate o 1000 pulando de 100 em 100
+
+print_r($arr2);
+echo "<br>";
+
+?>
